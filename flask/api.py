09efa525571246f8e628d2e7f65b742e77487a41
Bruce Sutherland <bruces1@gmail.com>
2024-11-29 15:06:56 +0800

    Largely working TODO app

--- flask/api.py
+++ flask/api.py
@@ -1,8 +1,50 @@
 import time
-from flask import Flask
+from flask import Flask, g, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = 'todo.sqlite3'
+
+def query_db(query, args=()):
+    cur = get_db().execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
 @app.route('/time')
-def get_time():
+def get_current_time():
     return {'time': time.time()}
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if not db:
+        db = g._database = sqlite3.connect(DATABASE, autocommit=True)
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exc):
+    db = getattr(g, '_database', None)
+    if db:
+        db.close()
+
+
+@app.get('/todo')
+def get_todos():
+    todos = query_db('SELECT id, text, done FROM todo ORDER BY id')
+    return [dict(row) for row in todos]
+
+
+@app.post('/todo')
+def add_todo():
+    text = request.json['text']
+    query_db('INSERT INTO todo(text) VALUES (?)', (text,))
+    return '', 201
+
+
+@app.put('/todo/<id>')
+def toggle_done(id):
+    query_db('UPDATE todo SET done=((done | 1) - (done & 1)) WHERE id=?', (id,))
+    return '', 204,
--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('/todo').then(res => res.json()).then(data => {
+      setTodos(data);
+    });
+  }, []);
+
+  const addTodo = () => {
+    fetch('/todo', {
+      method: 'POST',
+      headers:  { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: todoText})
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ol>
+        {todos.map(todo => <ToDo todo={todo} />)}
+        <li>
+          <input
+            type="text"
+            name="todoText"
+            onChange={ev => setTodoText(ev.target.value)}
+          />
+          <button onClick={(e) => addTodo()}>Add</button>
+        </li>
+      </ol>
     </div>
   );
 }
+
+function ToDo({ todo }) {
+  const toggleDone = (id) => {
+    fetch(`/todo/${id}`, {
+      method: 'PUT'
+    });
+  }
+
+  return <li
+    key={todo.id}
+    onClick={() => toggleDone(todo.id)}
+    style={{
+      textDecoration: todo.done ? 'line-through' : 'none'
+    }}
+  >
+    {todo.text}
+  </li>;
+};
 
 export default App;
